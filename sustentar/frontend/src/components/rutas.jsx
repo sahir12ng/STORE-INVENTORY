36be95b36c457ddd48199ce9458ffb09cd8aabb0
sahir12ng <118396725+sahir12ng@users.noreply.github.com>
2023-12-04 00:43:37 -0500

    Se elimina todas las carpetas

--- sustentar/frontend/src/components/rutas.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Inicio from "./inicio";
-import Registrar from "./registrar";
-import { useState } from "react";
